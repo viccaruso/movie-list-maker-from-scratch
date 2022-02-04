600f425af63c19899078814d0103d0be2b2632be
Victor Caruso <vic.caruso@gmail.com>
2022-02-04 11:57:31 -0800

    set up state in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [allMovies, setAllMovies] = useState([]);
+  const [filteredMovies, setFilteredMovies] = useState([]);
+  const [filterQuery, setFilterQuery] = useState('');
+  const [movieTitle, setMovieTitle] = useState('');
+  const [movieYear, setMovieYear] = useState('');
+  const [movieDirector, setMovieDirector] = useState('');
+  const [movieColor, setMovieColor] = useState('');
+  const [movieFormTitle, setMovieFormTitle] = useState([]);
+  const [movieFormYear, setMovieFormYear] = useState('');
+  const [movieFormDirector, setMovieFormDirector] = useState('');
+  const [movieFormColor, setMovieFormColor] = useState('');
+
+
   return (
     <div className="App">
       
