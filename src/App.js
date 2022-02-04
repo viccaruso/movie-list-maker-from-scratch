062492271c56b68ce9af6e869d4209c2a57dec2d
Victor Caruso <vic.caruso@gmail.com>
2022-02-04 13:00:32 -0800

    added props to MovieForm component in App.js and defined addMovie function

--- src/App.js
+++ src/App.js
@@ -6,19 +6,31 @@ function App() {
   const [allMovies, setAllMovies] = useState([]);
   const [filteredMovies, setFilteredMovies] = useState([]);
   const [filterQuery, setFilterQuery] = useState('');
-  const [movieTitle, setMovieTitle] = useState('');
-  const [movieYear, setMovieYear] = useState('');
-  const [movieDirector, setMovieDirector] = useState('');
-  const [movieColor, setMovieColor] = useState('');
+  // const [movieTitle, setMovieTitle] = useState('');
+  // const [movieYear, setMovieYear] = useState('');
+  // const [movieDirector, setMovieDirector] = useState('');
+  // const [movieColor, setMovieColor] = useState('');
   const [movieFormTitle, setMovieFormTitle] = useState([]);
   const [movieFormYear, setMovieFormYear] = useState('');
   const [movieFormDirector, setMovieFormDirector] = useState('');
-  const [movieFormColor, setMovieFormColor] = useState('');
+  const [movieFormColor, setMovieFormColor] = useState('red');
 
+  function addMovie(movie) {
+    setAllMovies([...allMovies, movie]);
+  }
 
   return (
     <div className="App">
-      <MovieForm />
+      <MovieForm
+        movieFormTitle={movieFormTitle}
+        movieFormDirector={movieFormDirector}
+        movieFormYear={movieFormYear}
+        movieFormColor={movieFormColor}
+        setMovieFormTitle={setMovieFormTitle}
+        setMovieFormDirector={setMovieFormDirector}
+        setMovieFormYear={setMovieFormYear}
+        setMovieFormColor={setMovieFormColor}
+        addMovie={addMovie} />
     </div>
   );
 }
